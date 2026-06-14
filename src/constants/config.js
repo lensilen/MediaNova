a41c366f8e84bfad6df4aaed1f30d9964fc01609
Ampasan <n.rakan.r.2005@gmail.com>
2026-06-14 18:29:49 +0700

    feat: setup Firebase config and initialize app connection

--- src/constants/config.js
+++ src/constants/config.js
@@ -6,3 +6,28 @@ export const firebaseConfig = {
   messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
 };
+
+export const requiredFirebaseConfigKeys = [
+  "apiKey",
+  "authDomain",
+  "projectId",
+  "storageBucket",
+  "messagingSenderId",
+  "appId",
+];
+
+export function getMissingFirebaseConfigKeys(config = firebaseConfig) {
+  return requiredFirebaseConfigKeys.filter((key) => !config[key]);
+}
+
+export function assertFirebaseConfig(config = firebaseConfig) {
+  const missingKeys = getMissingFirebaseConfigKeys(config);
+
+  if (missingKeys.length > 0) {
+    throw new Error(
+      `Konfigurasi Firebase belum lengkap. Cek .env untuk: ${missingKeys.join(
+        ", "
+      )}`
+    );
+  }
+}
--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,1 +1,14 @@
-export {};
+import { initializeApp, getApps } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+import { assertFirebaseConfig, firebaseConfig } from "../constants/config";
+
+assertFirebaseConfig();
+
+export const firebaseApp =
+  getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
+
+export const auth = getAuth(firebaseApp);
+export const db = getFirestore(firebaseApp);
+export const storage = getStorage(firebaseApp);
