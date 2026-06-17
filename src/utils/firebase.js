766641bf7024becdd2aad75caed20e32d8809615
Zaid Haikal Rizqi Kartawidjaja <zaid99haikal@gmail.com>
2026-06-17 22:51:50 +0700

    update tampilan dan fitur dari login dan register screen

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -9,7 +9,6 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 import { assertFirebaseConfig, firebaseConfig } from "../constants/config";
 import asyncStorage from "@react-native-async-storage/async-storage";
-import { initializeAuth, getReactNativePersistence } from "firebase/auth";
 
 assertFirebaseConfig();
 
